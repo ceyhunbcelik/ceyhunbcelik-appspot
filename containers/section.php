19e7b61867199dd4370775ffa84dd013334b5db6
ceyhunbcelik <ceyhunbcelik@icloud.com>
2021-03-04 17:18:35 +0300

    section-skills

--- containers/section.php
+++ containers/section.php
@@ -1,1 +1,18 @@
-<h1>SECTION</h1>
+<div class="row1">
+  <p>• I am newly graduated as Computer Engineer who developing Back-end&Front-end in Web but
+improved myself more at Back-end.</p>
+  <p>• I love technologies that can easily touch the life of people that’s why I want to work with
+experienced developers in people-oriented Web projects.</p>
+  <p>• I always been respectful and patient team player, aimed develop user friendly softwares and make
+my teammates can continue to develop product independently from me.</p>
+</div>
+
+<h2>SKILLS</h2>
+<div class="section_line"></div>
+
+<div class="row2">
+  <p><span>Web Development: </span>HTML&CSS, JS, Node.js, Express.js, React.js, Next.js</p>
+  <p><span>Languages: </span>PHP, C#</p>
+  <p><span>Databases: </span> MySQL, MongoDB, Microsoft SQL Server</p>
+  <p><span>Others: </span> Git, AWS, Heroku, Postman</p>
+</div>
