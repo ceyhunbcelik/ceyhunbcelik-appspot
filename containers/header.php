857addf3dbec56a3a2075fbecdb952f614eb1551
ceyhunbcelik <ceyhunbcelik@icloud.com>
2021-03-04 17:06:47 +0300

    header

--- containers/header.php
+++ containers/header.php
@@ -4,9 +4,9 @@
 </div>
 
 <div class="col2">
-  <p>ceyhunbcelik@icloud.com</p>
-  <p>+90 541 582 21 31</p>
-  <p>Ankara, Turkey</p>
-  <p>github.com/ceyhunbcelik</p>
-  <p>linkedin.com/in/ceyhunbcelik</p>
+  <p><a href="mailto:ceyhunbcelik@icloud.com">ceyhunbcelik@icloud.com</a></p>
+  <p><a href="facetime:+90 541 582 21 31">+90 541 582 21 31</a></p>
+  <p><a href="https://www.google.com/maps/place/Ankara/@39.9035557,32.6226818,11z/data=!3m1!4b1!4m5!3m4!1s0x14d347d520732db1:0xbdc57b0c0842b8d!8m2!3d39.9333635!4d32.8597419" target="_blank">Ankara, Turkey</a></p>
+  <p><a href="https://github.com/ceyhunbcelik" target="">github.com/ceyhunbcelik</a></p>
+  <p><a href="https://www.linkedin.com/in/ceyhunbcelik/" target="_blank">linkedin.com/in/ceyhunbcelik</a></p>
 </div>
