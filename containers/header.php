20f8f80e3ebea65932c0f758527347e1550c7411
ceyhunbcelik <ceyhunbcelik@icloud.com>
2021-03-04 16:42:49 +0300

    file structure

---
+++ containers/header.php
@@ -0,0 +1,1 @@
+<h1>CONTAINER</h1>
--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-<?php echo "CEYHUNBASE APPSPOT"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <!-- Meta Starts Here -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Meta Ends Here -->
+
+    <!-- CSS Starts Here -->
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/section.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <!-- CSS Ends Here -->
+
+    <!-- Title Starts Here -->
+    <title>Ceyhun Bahadır Çelik</title>
+    <!-- Title Ends Here -->
+
+  </head>
+  <body>
+
+    <!-- Main Starts Here -->
+    <main>
+
+      <!-- Header Starts Here -->
+      <?php require_once('./containers/header.php') ?>
+      <!-- Header Ends Here -->
+
+      <!-- Section Starts Here -->
+      <?php require_once('./containers/section.php') ?>
+      <!-- Section Ends Here -->
+
+      <!-- Footer Starts Here -->
+      <?php require_once('./containers/footer.php') ?>
+      <!-- Footer Ends Here -->
+
+    </main>
+    <!-- Main Ends Here -->
+
+
+  </body>
+</html>
